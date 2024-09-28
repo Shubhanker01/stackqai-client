4a22e824d9837eac0315debeb9d088e2af137081
Shubhanker01 <shubhanker40@gmail.com>
2024-09-28 13:53:49 +0530

    modifiedsignupform

--- project-app/src/Components/Signup.jsx
+++ project-app/src/Components/Signup.jsx
@@ -46,17 +46,30 @@ export default function Signup() {
 
                 <div className="w-full max-w-xs m-auto mt-[50px]">
                     <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                        <div className="mb-4">
+                        <div className="mb-2">
                             <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+                                Name
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="" />
+                        </div>
+                        <div className="mb-2">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" for="email">
                                 Email
                             </label>
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="email" placeholder="" />
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="" />
                         </div>
-                        <div className="mb-6">
+                        <div className="mb-2">
                             <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
                                 Password
                             </label>
                             <input className="shadow appearance-none border border-black-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="" />
+
+                        </div>
+                        <div className="mb-2">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" for="conf-password">
+                                Confirm Password
+                            </label>
+                            <input className="shadow appearance-none border border-black-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="conf-password" type="password" placeholder="" />
 
                         </div>
                         <div className="w-[256px]">
