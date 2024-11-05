720cb836212a3b6d58cdb15357dfb2e651d0eb7b
Shubhanker01 <shubhanker40@gmail.com>
2024-11-05 12:53:16 +0530

    History modified

--- project-app/src/Components/Results.jsx
+++ project-app/src/Components/Results.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from 'react-bootstrap/Card';
 function Results(props) {
-    return (
-        <>
-          <Card>
-            <Card.Title>Found 0 results for {props.search}</Card.Title>
-          </Card>
-        </>
-    )
+  const [results, setResults] = useState([])
+  useEffect(() => {
+
+
+  }, [])
+  return (
+    <>
+      <Card>
+        <Card.Title className='p-2'>Found 0 results for {props.search}</Card.Title>
+      </Card>
+      <div>
+        {
+          props.items.map((item) => {
+            return item.arr.map((obj) => {
+              return <div key={obj._id}>
+                <p>{obj.question}</p>
+                <p>{obj.answer}</p>
+              </div>
+            })
+          })
+        }
+      </div>
+    </>
+  )
 }
 
 export default Results
